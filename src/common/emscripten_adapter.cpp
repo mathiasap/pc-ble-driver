b1ceac591e71c89b85eb674d1a931ac2acddbfa7
Mathias Aarseth Pedersen <Mathias.Pedersen@nordicsemi.no>
2018-03-16 18:37:14 +0100

    Emscripten binding for opening adapter

--- src/common/emscripten_adapter.cpp
+++ src/common/emscripten_adapter.cpp
@@ -10,12 +10,13 @@ static void sd_rpc_on_status(adapter_t *adapter, sd_rpc_app_status_t id, const c
 
 static void sd_rpc_on_event(adapter_t *adapter, ble_evt_t *event)
 {
-    
+    emscripten_run_script("alert('event handler!')");
+
 }
 
 void sd_rpc_on_log_event(adapter_t *adapter, sd_rpc_log_severity_t severity, const char *log_message)
 {
-    
+
 }
 
 using DebugFunction = std::shared_ptr<std::function<uint32_t(const char*)>>;
@@ -48,7 +49,9 @@ const char *createEmscriptenDebugText(T t, U... u)
     return stream.str().c_str();
 }
 
-
+extern "C" {
+  extern void my_js(const char* str);
+}
 std::vector<std::unique_ptr<std::thread>> adapterThreads;
 
 void openAdapterAsync(void/*std::function<void(void)> f*/)
@@ -63,26 +66,28 @@ void openAdapterAsync(void/*std::function<void(void)> f*/)
 
     //auto error_code = sd_rpc_log_handler_severity_filter_set(adapter, 0);
     auto error_code = sd_rpc_open(adapter, sd_rpc_on_status, sd_rpc_on_event, sd_rpc_on_log_event);
-    
+
     //Does not work because function pointer table is not available in thread
-    const char* out = "test";
-    (*emscriptenDebugOut)(out);
+    my_js(createEmscriptenDebugText("result from open: ",error_code));
+    //(*emscriptenDebugOut)(out);
 
 }
 
 
-#include <stdio.h>
 extern "C" {
 
 	EMSCRIPTEN_KEEPALIVE
-    int openAdapter()
+    int emscriptenOpenAdapter()
     {
-        /*std::unique_ptr<std::thread> t(new std::thread(openAdapterAsync, [&](){
-            (*emscriptenDebugOut)(createEmscriptenDebugText("Test"));
-        }));*/
-        std::unique_ptr<std::thread> t(new std::thread(openAdapterAsync));
-        adapterThreads.push_back(std::move(t));
-        (*emscriptenDebugOut)(createEmscriptenDebugText("Created a new adapter thread"));
+        int retransmission_interval = 5000;
+        int response_timeout = 5000;
+
+        auto webusb = sd_rpc_physical_layer_create_webusb();
+        auto h5 = sd_rpc_data_link_layer_create_bt_three_wire(webusb, retransmission_interval);
+        auto serialization = sd_rpc_transport_layer_create(h5, response_timeout);
+        auto adapter = sd_rpc_adapter_create(serialization);
+
+        auto error_code = sd_rpc_open(adapter, sd_rpc_on_status, sd_rpc_on_event, sd_rpc_on_log_event);
         return 0;
     }
 }
