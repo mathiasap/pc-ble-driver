ba84a7a58ba38eaf7712da82bb34db288440190f
Mathias Aarseth Pedersen <Mathias.Pedersen@nordicsemi.no>
2018-03-16 18:36:20 +0100

    Updated build script

--- build-link.bat
+++ build-link.bat
@@ -1,7 +1,4 @@
-mingw32-make && em++ -o libpc_ble_driver_static_sd_api_v5.js libpc_ble_driver_static_sd_api_v5.a --js-library pre_js.js ^
--s USE_PTHREADS=2 ^
--s DEMANGLE_SUPPORT=1 ^
+mingw32-make && em++ -o src/js/pcbleblob.js libpc_ble_driver_static_sd_api_v5.a --pre-js src/js/class_bindings.js ^
 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'getValue','Pointer_stringify', 'intArrayFromString', 'cwrap', 'addFunction']" ^
 -s VERBOSE=1 ^
--s RESERVED_FUNCTION_POINTERS=32 ^
--s ALIASING_FUNCTION_POINTERS=0
+-s RESERVED_FUNCTION_POINTERS=32
