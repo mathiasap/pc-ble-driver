529f507dc28da33c6c62ab9abc14df3cf45c867b
Mathias Aarseth Pedersen <Mathias.Pedersen@nordicsemi.no>
2018-03-16 18:34:07 +0100

    Interface will open adapter from c++

--- src/js/interface.js
+++ src/js/interface.js
@@ -12,15 +12,20 @@ function dataCallback(data, length){
 }
 
 async function openAdapter(){
-    const webusb = new WebusbInterface();
-    const h5 = new H5Transport(webusb, 5000);
-    const serialization = new SerializationTransport(h5, 5000);
+    //const webusb = new WebusbInterface();
+    //const h5 = new H5Transport(webusb, 5000);
+    //const serialization = new SerializationTransport(h5, 5000);
 
 
-    console.log("opening");
-    let res = await serialization.open(statusCallback, dataCallback, logCallback)
-    console.log("Back to interface");
+    //console.log("opening");
+    //let res = await serialization.open(statusCallback, dataCallback, logCallback)
+    //console.log("Back to interface");
+    //console.log(res)
+    console.log("Attempting to open adapter")
+    let res = Module.ccall('emscriptenOpenAdapter', 'number', [], []);
     console.log(res)
+    console.log("Open adapter attempt done")
+
 
 
 }
